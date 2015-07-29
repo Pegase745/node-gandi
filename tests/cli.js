39f836afe2b083be711d18279c70ca1531701ba2
Michel Nemnom <michel.nemnom@gmail.com>
2015-07-29 21:27:38 +0200

    Add functional tests to OT&E

--- tests/cli.js
+++ tests/cli.js
@@ -6,6 +6,7 @@ var _ = require('lodash');
 
 var cliPath = path.resolve(__dirname, '../src/gandi.js');
 var cli;
+var gandi;
 
 
 /**
@@ -57,18 +58,18 @@ describe('Gandi client', function () {
         delete require.cache[cliPath];
     });
 
-    it('should return an error when no arguments are passed', function() {
+    it('should throw an error when no arguments are passed', function() {
         assert.throws(function() {new cli()}, TypeError);
     });
 
-    it('should return an error when apikey is not 24 characters long', function() {
+    it('should throw an error when apikey is not 24 characters long', function() {
         var bad_apikey = '1234erty';
 
         assert.throws(function() {new cli(bad_apikey)}, TypeError);
     });
 
     it('should be ok when apikey and not ote', function() {
-        var gandi = new cli(apikey);
+        gandi = new cli(apikey);
 
         assert.equal(gandi.apikey, apikey);
         assert.ok(_.isEqual(gandi.config, config_prod));
@@ -76,10 +77,66 @@ describe('Gandi client', function () {
     });
 
     it('should be ok when apikey and ote', function() {
-        var gandi = new cli(apikey, true);
+        gandi = new cli(apikey, true);
 
         assert.equal(gandi.apikey, apikey);
         assert.ok(_.isEqual(gandi.config, config_ote));
         assert.ok(gandi.client);
+    });
+});
+
+
+/**
+ * Describe Gandi OTE query tests
+ */
+describe('Gandi OT&E query', function () {
+
+    before(function () {
+        cli = require('../src/gandi.js');
+        gandi = new cli(apikey, true);
+    });
+
+    after(function () {
+        cli = null;
+        delete require.cache[cliPath];
+    });
+
+    it('should be ok when a request is made and it doesn\'t need params', function(done) {
+        gandi.call('version.info', [], function(err, data) {
+            assert.ifError(err);
+            assert.ok('api_version' in data);
+            assert.notEqual(data.api_version, null);
+            done();
+        });
+    });
+
+    it('should be ok when a request is made with params passed while not needed', function(done) {
+        gandi.call('version.info', ['toto', 'titi'], function(err, data) {
+            assert.ifError(err);
+            assert.ok('api_version' in data);
+            assert.notEqual(data.api_version, null);
+            done();
+        });
+    });
+
+    it('should throw a `method not supported` error when calling a non-existant API method', function(done) {
+        gandi.call('version.detail', [], function(err, data) {
+            assert.equal(err['faultCode'], 150020);
+            done();
+        });
+    });
+
+    it('should throw a `invalid method parameter(s)` error when calling with bad param type', function(done) {
+        gandi.call('domain.available', ['pegase745.org'], function(err, data) {
+            assert.equal(err['faultCode'], 500037);
+            done();
+        });
+    });
+
+    it('should throw a `takes at least X arguments` error when calling with bad param number', function(done) {
+        gandi.call('domain.available', [], function(err, data) {
+            assert.equal(err['faultCode'], 150020);
+            done();
+        });
     });
 });
